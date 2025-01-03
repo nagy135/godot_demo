2ea5ea9f44f514efb20a5c214887911a5ac17f55
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-01-03 11:22:42 +0100

    feat: reset position of player

--- mover.gd
+++ mover.gd
@@ -2,8 +2,8 @@ extends CharacterBody3D
 
 
 const SPEED = 25.0
-const JUMP_VELOCITY = 9.5
-const GRAVITY_MULTIPLIER = 2
+const JUMP_VELOCITY = 20.5
+const GRAVITY_MULTIPLIER = 4
 
 
 func _physics_process(delta: float) -> void:
@@ -12,6 +12,8 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_key_pressed(KEY_Q):
+		position = Vector3.ZERO
 
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
